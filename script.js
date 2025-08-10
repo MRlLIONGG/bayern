3de9c5b278345aa78b1556e857d792b65d0e47f7
XxAdamxX28 <126411222+MRlLIONGG@users.noreply.github.com>
2025-08-10 21:50:40 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// For now, no scripts are necessary.
+// But here you can add future interactivity if you want,
+// like showing alerts, or interactive effects.
+
+console.log("Welcome to your Bayern Munich players info page!");
